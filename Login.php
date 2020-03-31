014ea42d6471166dfbcecacaa91d47c474ed256f
David <sky@Daves-MBP.fritz.box>
2020-03-31 11:32:10 +0200

    login html

--- Login.php
+++ Login.php
@@ -3,15 +3,27 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
+    <title>Login Page</title>
 </head>
 <body>
-<p>This is new I hope this is going to work</p>
-    
+<p>Welcome to login page</p>
+    <form action="Login.php" method="post">
+    Username: <input type="text" name="username" required>
+    Password: <input type="text" name="password" required>
+    Login: <input type="submit" name="login" value="Login">
+    </form>
 
 </body>
 </html>
 <?php
-// cool !! Now fill this in ! 
-// gz !
+if(isset($_POST['username'])&& isset($_POST['password']))   {
+    include_once('credentials.php');
+    $connect = mysqli_connect($servername,$username,$password,$database);
+
+    if($connect!==$connect){
+        die(' Unsuccessful connection' . mysqli_connect_errno());
+    }
+
+
+}
 ?>
