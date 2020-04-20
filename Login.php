9dcf0b146f514e6811ae9df2689f5f2219954c6b
David <sky@Daves-MBP.fritz.box>
2020-04-20 16:09:03 +0200

    session

--- Login.php
+++ Login.php
@@ -1,27 +1,42 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login Page</title>
-</head>
-
-<body>
-    <p>Welcome to login page</p>
-
-
-</body>
-
-</html>
 <?php
-if (isset($_POST['username']) && isset($_POST['password'])) {
-    include_once('credentials.php');
-    $connect = mysqli_connect($servername, $username, $password, $database);
+include_once 'sessionCheck.php';
+include_once 'credentials.php';
+function displayuser($connect)
+{
+    $user = $connect->prepare('SELECT * FROM ppl WHERE PERSON_ID=?');
+    $user->bind_param('i', $_SESSION['UserId']);
+    $user->execute();
+    $MyResult = $user->get_result();
+    $row = $MyResult->fetch_assoc();
 
-    if (!$connect) {
-        die(' Unsuccessful connection' . mysqli_connect_errno());
-    }
+    print ' First Name: ' . $row['First_Name'] . '</br>';
+    print ' Last Name: ' . $row['Second_Name'] . '</br>';
+    print ' Age: ' . $row['Age'] . '</br>';
+    print ' User Name: ' . $row['UserName'] . '</br>';
+    $country = $connect->prepare('SELECT * FROM COUNTRIES WHERE COUNTRY_ID=?');
+    $country->bind_param('i', $row['Nationality']);
+    $country->execute();
+    $resultOfCountry = $country->get_result();
+    $nationality = $resultOfCountry->fetch_assoc();
+    print ' You are from: ' . $nationality["COUNTRY_NAME"];
+?>
+    <form action="Login.php" method="post">
+
+        <input type="submit" name="Logout" value="Logout">
+    </form>
+    <?php
+
+}
+if (isset($_POST['Logout'])) {
+    session_destroy();
+    session_unset();
+    print ' You are loggout';
+} elseif ($_SESSION['UserLog']) {
+    print ' You are already logged in';
+    displayuser($connect);
+} elseif (isset($_POST['username']) && isset($_POST['password'])) {
+
+
     $user = $connect->prepare('SELECT * FROM ppl WHERE username=?');
     $user->bind_param('s', $_POST['username']);
     $user->execute();
@@ -32,23 +47,18 @@ if (isset($_POST['username']) && isset($_POST['password'])) {
         $row = $MyResult->fetch_assoc();
 
         if (password_verify($_POST['password'], $row['Password'])) {
-            print ' Successfully Registered</br>';
-            print ' First Name: ' . $row['First_Name'] . '</br>';
-            print ' Last Name: ' . $row['Second_Name'] . '</br>';
-            print ' Age: ' . $row['Age'] . '</br>';
-            print ' User Name: ' . $row['UserName'] . '</br>';
-            $country = $connect->prepare('SELECT * FROM COUNTRIES WHERE COUNTRY_ID=?');
-            $country->bind_param('i', $row['Nationality']);
-            $country->execute();
-            $resultOfCountry = $country->get_result();
-            $nationality = $resultOfCountry->fetch_assoc();
-            print ' You are from: '.$nationality["COUNTRY_NAME"];
+
+            $_SESSION['UserLog'] = true;
+
+
+            $_SESSION['UserId'] = $row['PERSON_ID'];
+            displayuser($connect);
         } else {
             print ' Mismatch password';
         }
     } else {
         print ' You are not in our database. Please register first';
-?>
+    ?>
         <a href="Singup.php">Singup page</a>
         <a href="Login.php">Login page</a>
     <?php
@@ -57,7 +67,7 @@ if (isset($_POST['username']) && isset($_POST['password'])) {
     ?>
     <form action="Login.php" method="post">
         Username: <input type="text" name="username" required>
-        Password: <input type="text" name="password" required>
+        Password: <input type="password" name="password" required>
         Login: <input type="submit" name="login" value="Login">
     </form>
 <?php
--- credentials.php
+++ credentials.php
@@ -3,4 +3,9 @@ $servername ="localhost";
 $username = "root";
 $password = "";
 $database = "mohsen";
+$connect = mysqli_connect($servername, $username, $password, $database);
+if (!$connect) {
+    die(' Unsuccessful connection' . mysqli_connect_errno());
+}
+
 ?>
